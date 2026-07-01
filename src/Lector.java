0745519737784c5e557721cbf015ff6d7f65269b
Roaa896 <gsroas.2020@gmail.com>
2026-07-01 12:07:34 -0500

    uploading All

---
+++ src/Lector.java
@@ -0,0 +1,40 @@
+public class Lector {
+    private String nombre;
+    private String cedula;
+    private Libro libroActual;  
+
+    public Lector(String nombre, String cedula){
+        this.nombre = nombre;
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getCedula(){
+        return this.cedula;
+    }
+
+    public Libro getlibro(){
+        return this.libroActual;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void tomarPrestado(Libro libro){
+        if (libroActual != null ) {
+           System.out.println(nombre + "ya tiene el libro \"" + libroActual.getTitulo() + "\".Debe devolverlo primero." ); 
+        } else { 
+            if (libro.prestar()) {
+                libroActual = libro;
+            }
+        }
+    }
+}
